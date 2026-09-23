b7d5066d8ce8cd894843c407d55e106659fc42de
ogdevelopers0-svg <ogdevelopers0@gmail.com>
2026-09-23 13:07:33 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,29 +1,22 @@
 import "./globals.css";
-import Script from "next/script";
 
 export const metadata = {
   title: "E-CITIZEN Kenya Services Hub",
-  description: "Independent service discovery hub linking users to official eCitizen services."
+  description:
+    "Independent service discovery hub linking users to official eCitizen services.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-  <meta
-    name="google-adsense-account"
-    content="ca-pub-4573252775101941"
-  />
+        <meta
+          name="juicyads-site-verification"
+          content="b91c92061b1df7c149ca5e3d2fcdc501"
+        />
+      </head>
 
-  <meta name="juicyads-site-verification" content="b91c92061b1df7c149ca5e3d2fcdc501">
-
-  <Script
-    async
-    strategy="beforeInteractive"
-    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4573252775101941"
-    crossOrigin="anonymous"
-  />
-</head>      <body>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
