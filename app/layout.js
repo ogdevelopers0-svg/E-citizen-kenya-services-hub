893a0c031d39c619c96c5221f9071f1ee4d72d26
ogdevelopers0-svg <ogdevelopers0@gmail.com>
2026-09-23 12:56:42 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -10,15 +10,23 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-    <meta name="google-adsense-account" content="ca-pub-4573252775101941" />
-        <Script
-          async
-          strategy="beforeInteractive"
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4573252775101941"
-          crossOrigin="anonymous"
-        />
-      </head>
-      <body>{children}</body>
+  <meta
+    name="google-adsense-account"
+    content="ca-pub-4573252775101941"
+  />
+
+  <meta
+    name="juicyads-site-verification"
+    content="09431bcfcf145b4c745accc269748707"
+  />
+
+  <Script
+    async
+    strategy="beforeInteractive"
+    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4573252775101941"
+    crossOrigin="anonymous"
+  />
+</head>      <body>{children}</body>
     </html>
   );
 }
